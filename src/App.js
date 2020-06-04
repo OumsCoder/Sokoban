148e6f7b240a7d82c135d89b7ab1096f077096a7
Oumar Cissé <66132522+OumsCoder@users.noreply.github.com>
2020-06-04 23:20:55 +0000

    sokoban

--- src/App.js
+++ src/App.js
@@ -13,18 +13,20 @@ class App extends Component {
       [0, 20],     [10, 80],   [90, 80],   [80, 0], 
       [10, 10],    [20, 80],   [90, 70],   [70, 0],
       [10, 20],    [30, 80],   [90, 60],   [60, 0],
-      [0, 30],     [40, 80],    [90, 50],    [50, 0],
-      [0, 40],     [50, 80],    [90, 40],    [40, 0],
-      [0, 50],     [60, 80],    [90, 30],    [30, 0],
-      [0, 60],     [70, 90],   [90, 20],    [20, 0],
-      [0, 70],     [80, 90],   [90, 10],    [10, 0],
-      [0, 80],     [90, 90],   [90, 0],     [50, 20],
-      [60, 90],     [30, 10],   [30, 20],    [40, 20],
+      [0, 30],     [40, 80],   [90, 50],   [50, 0],
+      [0, 40],     [50, 80],   [90, 40],   [40, 0],
+      [0, 50],     [60, 80],   [90, 30],   [30, 0],
+      [0, 60],     [70, 90],   [90, 20],   [20, 0],
+      [0, 70],     [80, 90],   [90, 10],   [10, 0],
+      [0, 80],     [90, 90],   [90, 0],    [50, 20],
+      [60, 90],    [30, 10],   [30, 20],   [40, 20],
     ],
     // Stance Player
     stancePlayer : [40, 10],
     // Stance Goal
-    stanceGoal : [60, 40]
+    stanceGoal : [60, 40],
+    // Stance Win
+    stanceWin : [70, 60]
   }
   
   // fx when user press up 
@@ -131,7 +133,8 @@ class App extends Component {
       <div className="game-area">
         <Player stance={this.state.stancePlayer} />
         <Stage area={this.state.level}/>
-        <Goal stances={this.state.stanceGoal}/>  
+        <Goal win={true} stances={this.state.stanceGoal}/> 
+        <Goal win={false} stances={this.state.stanceWin}/> 
       </div>
     )
   }
--- src/Components/Goal/Goal.js
+++ src/Components/Goal/Goal.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import goal from './gol.jpg'
+import goal1 from './goal.jpg'
 import './Goal.css'
 
-function Goal({stances}) {
+
+function Goal({win,stances}) {
+   if(win){
     return <img id="goal" style={{top :`${stances[0]}%` , left :`${stances[1]}%`}} src={goal} alt="goal"></img>
+   }else{
+    return <img id="goal" style={{top :`${stances[0]}%` , left :`${stances[1]}%`}} src={goal1} alt="goal"></img>
+   }
+   
 }
 
 export default Goal
